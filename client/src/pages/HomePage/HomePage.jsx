8d5d2a319400b469c3e459f3944b5b2064ea8477
Sheila Ho <ho.sheila@hotmail.com>
2023-04-09 19:19:35 -0400

    feat: added button styles and added content to homepage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -7,9 +7,21 @@ export default function HomePage() {
     <>
       {/* <MobileNav />
       <NavBar /> */}
-      <main className="homapage">
+      <main className="homepage">
         <div className="homepage__wrapper">
-          <img className="homepage__banner" src={banner} />
+          <div className="homepage__container">
+            <div className="homepage__bannercontain">
+              <img className="homepage__banner" src={banner} />
+            </div>
+            <div className="homepage__content">
+              <p className="homepage__title">Hello, User Name</p>
+              <div className="homepage__active">
+                <p className="homepage__subtitle">Active Session</p>
+                <p className="homepage__time">Time Duration: 00:00</p>
+              </div>
+              <button className="homepage__button">Book Now</button>
+            </div>
+          </div>
         </div>
       </main>
     </>
