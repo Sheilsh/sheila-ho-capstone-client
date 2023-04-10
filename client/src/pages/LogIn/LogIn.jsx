fd1287bd591aa98d9f44928c3f2e1b1babb49b76
Sheila Ho <ho.sheila@hotmail.com>
2023-04-09 20:09:00 -0400

    feat: added login form to login page

--- client/src/pages/LogIn/LogIn.jsx
+++ client/src/pages/LogIn/LogIn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Login.scss";
+import "./LogIn.scss";
 import banner from "../../assets/logo/logo_transparent.png";
 
 export default function LogIn() {
@@ -7,48 +7,30 @@ export default function LogIn() {
     <main className="login">
       <div className="login__wrapper">
         <div className="login__formcontain">
-          <div className="login">
-            <form action="/getstarted" className="form">
-              <img src={banner} />
-              <p>Please Sign In</p>
-              <div className="input-box">
-                <input typeOf="email" name="email" required />
-                <label className="form__info">Email</label>
-                <i></i>
-              </div>
-              <div class="input-box">
-                <input typeOf="password" name="password" required />
-                <label className="form__info">Password</label>
-                <i></i>
-              </div>
-              <button className="login__button login__button-login">
-                Log In
-              </button>
-              <button className="login__button login__button-create">
-                Create Account
-              </button>
-            </form>
-          </div>
+          <form action="/" className="login__form">
+            <div className="login__bannercontain">
+              <img className="login__banner" src={banner} />
+            </div>
+            {/* <p>Please Sign In</p> */}
+            <div className="input-box">
+              <input typeof="email" name="email" required />
+              <label className="form__info">Email</label>
+              <i></i>
+            </div>
+            <div className="input-box">
+              <input typeof="password" name="password" required />
+              <label className="form__info">Password</label>
+              <i></i>
+            </div>
+            <button className="login__button login__button-login">
+              Log In
+            </button>
+            <button className="login__button login__button-create">
+              Create Account
+            </button>
+          </form>
         </div>
       </div>
     </main>
   );
 }
-
-//  <div className="login">
-//     <form action="/getstarted" className="form">
-//       <h1>Log In</h1>
-//       <p>Please Sign In</p>
-//       <div className="input-box">
-//         <input typeOf="email" name="email" required />
-//         <label className="form__info">Email</label>
-//         <i></i>
-//       </div>
-//       <div class="input-box">
-//         <input typeOf="password" name="password" required />
-//         <label className="form__info">Password</label>
-//         <i></i>
-//       </div>
-//       <button className="btn">Submit</button>
-//     </form>
-//   </div>
