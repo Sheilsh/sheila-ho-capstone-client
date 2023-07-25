039f13810a8708c91773b61cc84c90b053386c84
Sheila Ho <ho.sheila@hotmail.com>
2023-07-25 19:43:01 -0400

    chore: removed comments from homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,85 +1,7 @@
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import { getUserById, getUserBooking } from "../utils/helpers";
-// import Home from "../components/Home/Home";
-
-// export default function HomePage() {
-//   const { id } = useParams();
-
-//   // -----  state -----
-//   const [loading, setLoading] = useState(true);
-//   const [userData, setUserData] = useState([]);
-//   const [activeBooking, setActiveBooking] = useState(null);
-
-//   // -----  useEffect/ apiData -----
-//   useEffect(() => {
-//     getUserById(id).then((data) => {
-//       setUserData(data);
-//       setLoading(false);
-//     });
-
-//     getUserBooking(id).then((data) => {
-//       const activeBooking = data.find((booking) => {
-//         const endTime = new Date(booking.end_datetime).getTime();
-//         const currentTime = new Date().getTime();
-//         return currentTime < endTime;
-//       });
-//       setActiveBooking(activeBooking);
-//     });
-//   }, [id]);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   } else {
-//     return <Home key={id} userData={userData} activeBooking={activeBooking} />;
-//   }
-// }
-
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getUserById, getUserBooking } from "../utils/helpers";
 import Home from "../components/Home/Home";
-
-// export default function HomePage() {
-//   const { id } = useParams();
-
-//   // -----  state -----
-//   const [loading, setLoading] = useState(true);
-//   const [userData, setUserData] = useState([]);
-//   const [activeBooking, setActiveBooking] = useState(null);
-
-//   // -----  useEffect/ apiData -----
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       setLoading(true);
-//       try {
-//         const userData = await getUserById(id);
-//         setUserData(userData);
-
-//         const bookingData = await getUserBooking(id);
-//         const activeBooking = bookingData.find((booking) => {
-//           const endTime = new Date(booking.end_datetime).getTime();
-//           const currentTime = new Date().getTime();
-//           return currentTime < endTime;
-//         });
-//         setActiveBooking(activeBooking);
-
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching user data:", error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, [id]);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   } else {
-//     return <Home key={id} userData={userData} activeBooking={activeBooking} />;
-//   }
-// }
 
 export default function HomePage() {
   const { id } = useParams();
@@ -94,11 +16,9 @@ export default function HomePage() {
     const fetchData = async () => {
       setLoading(true);
       try {
-        // const userData = await getUserById(id);
         const userData = await getUserById(id + `?timestamp=${Date.now()}`);
         setUserData(userData);
 
-        // const bookingData = await getUserBooking(id);
         const bookingData = await getUserBooking(
           id + `?timestamp=${Date.now()}`
         );
